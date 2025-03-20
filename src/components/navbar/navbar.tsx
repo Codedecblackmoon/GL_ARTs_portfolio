093f634241758eaf30c6c6484899110d4efe1da4
Codedecblackmoon <gideonlenge96@gmail.com>
2025-03-20 15:09:28 +0200

    update

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,20 +1,10 @@
-// trypscript short cut for functlion
-
 export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="#">
                 Navbar
             </a>
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-toggle="collapse"
-                data-target="#navbarNav"
-                aria-controls="navbarNav"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
+            <button >
                 <span className="navbar-toggler-icon"></span>
             </button>
         </nav>  
