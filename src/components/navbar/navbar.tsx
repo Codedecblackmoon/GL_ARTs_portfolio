e755c700be049dad895af7320b70322483119725
Codedecblackmoon <gideonlenge96@gmail.com>
2025-03-24 21:55:14 +0200

    update

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,12 +2,11 @@ export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container">
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span><i  id="bar" className="fa-solid fa-bars"></i></span>
-                </button>
+                </button> */}
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ml-auto">
-                    
                         <li className="nav-item">
                             <a className="nav-link" href="index.html">Home</a>
                         </li>
