2b5b4a49186a6f2c79f12ebdfde3714b7e868242
Codedecblackmoon <gideonlenge96@gmail.com>
2025-03-24 21:54:22 +0200

    update

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,12 +1,31 @@
 export const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">
-                Navbar
-            </a>
-            <button >
-                <span className="navbar-toggler-icon"></span>
-            </button>
+            <div className="container">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span><i  id="bar" className="fa-solid fa-bars"></i></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                    
+                        <li className="nav-item">
+                            <a className="nav-link" href="index.html">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="shop.html">Shop</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="blog.html">Blog</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="About_us.html">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="Contact_us.html">Contact us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>  
         )
     };
