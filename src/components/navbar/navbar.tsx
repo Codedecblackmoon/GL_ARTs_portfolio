441a9b76751382d203bede46c27640ea3384bbaa
Codedecblackmoon <gideonlenge96@gmail.com>
2025-03-28 21:29:12 +0200

    update

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -19,7 +19,7 @@ export const Navbar = () => {
                             <a className="nav-link" href="About_us.html">About</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="Contact_us.html">Contact us</a>
+                            <a className="nav-link" href="Contact_us.htmlddd">Contact us</a>
                         </li>
                     </ul>
                 </div>
