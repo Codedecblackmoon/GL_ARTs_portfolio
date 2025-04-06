018953093a754a8068fb49ca33f62731dacbb1c1
Codedecblackmoon <gideonlenge96@gmail.com>
2025-04-06 21:44:20 +0200

    update

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -2,29 +2,18 @@ import './nav.css'
 
 export const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container">
-                {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span><i  id="bar" className="fa-solid fa-bars"></i></span>
-                </button> */}
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ml-auto">
-                        <li className="nav-item">
-                            <a className="nav-link" href="index.html">Home</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="shop.html">Shop</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="About_us.html">About</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="Contact_us.html">Contact us</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>  
+        <div>
+            <h1>GL ARTs</h1>
+            <nav>
+                <ul>
+                    <li><a>Home</a></li>
+                    <li><a>Work</a></li>
+                    <li><a>About</a></li>
+                    <li><a>Contact US</a></li>
+                </ul>
+            </nav> 
+        </div>
+        
         )
     };
 
